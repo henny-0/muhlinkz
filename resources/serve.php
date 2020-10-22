27053cfd662fcd183b1811b61820bc598a0c4881
henny-0 <47804901+henny-0@users.noreply.github.com>
2020-10-22 17:24:21 -0500

    Create serve.php

---
+++ resources/serve.php
@@ -0,0 +1,9 @@
+<?php
+    //error_reporting(0);
+    $servername = "localhost";
+    $dbname = "linkdb";
+    $dbname2 = "linkdb2";
+    $tbl = "linktbl";
+    $uname = "admin";
+    $pwd = "password";
+?>
